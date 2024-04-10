10a3c1547d888c4487de89ab38fbb8a1723a5ba9
Elias Shemsu <echosierran@gmail.com>
2024-04-10 12:09:13 +0300

    EnterTokenDialog

--- app/src/main/java/com/example/firebasepushnotifications/ui/EnterTokenDialog.kt
+++ app/src/main/java/com/example/firebasepushnotifications/ui/EnterTokenDialog.kt
@@ -1,12 +1,20 @@
 package com.example.firebasepushnotifications.ui
 
+import android.widget.Toast
 import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Button
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedButton
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -15,9 +23,14 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.platform.LocalClipboardManager
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.text.AnnotatedString
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.window.Dialog
 import androidx.compose.ui.window.DialogProperties
+import com.google.firebase.ktx.Firebase
+import com.google.firebase.messaging.ktx.messaging
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.tasks.await
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
@@ -54,6 +67,32 @@ fun EnterTokenDialog(
                 },
                 maxLines = 1,
             )
+            Spacer(modifier = Modifier.height(16.dp))
+            Row(
+                modifier = Modifier
+                    .fillMaxWidth(),
+                horizontalArrangement = Arrangement.End
+            ) {
+                OutlinedButton(
+                    onClick = {
+                    scope.launch {
+                        val localToken = Firebase.messaging.token.await()
+                        clipboardManager.setText(AnnotatedString(localToken))
+
+                        Toast.makeText(
+                            context,
+                            "Copied to clipboard",
+                            Toast.LENGTH_SHORT
+                        ).show()
+                    }
+                }) {
+
+                }
+                Spacer(modifier = Modifier.width(16.dp))
+                Button(onClick = onSubmit) {
+                    Text(text = "Submit")
+                }
+            }
         }
     }
 }
